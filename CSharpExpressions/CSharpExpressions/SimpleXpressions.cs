5a344345bb51b5ebbfd10246fb227c53302b24a5
eduard.kobzik <eduard.kobzik@gmail.com>
2020-02-07 18:14:14 +0300

    init proj

---
+++ CSharpExpressions/CSharpExpressions/SimpleXpressions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace CSharpExpressions
+{
+    public class SimpleXpressions
+    {
+        public void Process()
+        {
+            Expression expr = Expression.Add(Expression.Constant(1), Expression.Constant(2));
+            var lambdaExpr = Expression.Lambda(expr);
+            var someOtherType = Expression.Invoke(expr);
+            
+            var compiledLambda = lambdaExpr.Compile();
+
+        }
+    }
+}
