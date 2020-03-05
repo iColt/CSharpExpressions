13ee38749b1ddcaeb44e211a74c1644e6f2196a3
eduard.kobzik <eduard.kobzik@gmail.com>
2020-03-05 16:22:25 +0300

    Entities and factory

--- CSharpExpressions/CSharpExpressions/Models/Order.cs
+++ CSharpExpressions/CSharpExpressions/Models/Order.cs
@@ -4,7 +4,32 @@ using System.Text;
 
 namespace CSharpExpressions.Models
 {
-    class Order
+    public class Order
     {
+        public int OrderId { get; set; }
+
+        public string Code { get; set; }
+
+        public string Notes { get; set; }
+
+        public int? CustomerId { get; set; }
+
+        public decimal NetAmount { get; set; }
+
+        public bool Accepted { get; set; }
+
+        public Order()
+        {
+
+        }
+
+        #region Factory
+
+        public Order GetOrder(int id)
+        {
+            return null;
+        }
+
+        #endregion
     }
 }
